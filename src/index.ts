024d5d823ba53e720bbcb941980bf092570c52ab
Ashu <Matheusn.Biolowons@gmail.com>
2025-06-10 16:13:29 -0300

    ❌ Removing a test embedded in the code

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,5 @@
-import { Analyze } from './controllers/Analyze'
-
 export * from './controllers/Analyze'
 export * from './controllers/Node'
 export * from './types/analyze'
 export * from './types/node'
 export * from './utils/decodeURL'
-
-const analyzer = new Analyze('/users/:id=number')
-const analyzed = new Analyze('/users/100', analyzer)
-
-console.log(analyzed.display())
-console.log(analyzed.getParams())
